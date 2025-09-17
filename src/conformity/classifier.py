ed4e67120d0d623a95eb096cd14737aed28ecf77
Jack Greenaway <103508368+JackGreenaway@users.noreply.github.com>
2025-09-17 20:29:34 +0100

    Update classifier.py

--- src/conformity/classifier.py
+++ src/conformity/classifier.py
@@ -11,24 +11,20 @@ class ConformalClassifier(BaseConformalPredictor):
         self.estimator = estimator
 
     def calibrate(self, X: ArrayLike, y: ArrayLike) -> None:
-        # predict class probabilities
         y_prob = self.estimator.predict_proba(X)
 
-        # select true value
         true_probs = y_prob[np.arange(y_prob.shape[0]), y]
 
-        # conformity measure - hinge loss in this case
         self.calibration_non_conformity = 1 - true_probs
         self.n_calib = self.calibration_non_conformity.shape[0]
 
         return None
 
     def predict(self, X: ArrayLike, alpha: float = 0.05) -> ArrayLike:
-        # predict class probabilities
         y_prob = self.estimator.predict_proba(X)
-        # conformity measure - hinge loss in this case
+
         non_conformity = 1 - y_prob
-        # compute empirical conformity distribution
+
         conformity_score = (
             self.calibration_non_conformity.shape[0]
             - np.searchsorted(
@@ -36,11 +32,11 @@ class ConformalClassifier(BaseConformalPredictor):
             )
             + 1
         ) / (self.n_calib + 1)
-        # calculate q_level
+        
         q_level = np.ceil((self.n_calib + 1) * (1 - alpha)) / self.n_calib
-        # boolean prediction set
+        
         boolean_set = conformity_score > (1 - q_level)
-        # create prediction set
+        
         pred_set = np.where(
             boolean_set, np.vstack([self.estimator.classes_] * X.shape[0]), np.nan
         )
