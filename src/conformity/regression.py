6a6b390cd9b44260ef6f7882c8cf6239a695589f
Jack Greenaway <103508368+JackGreenaway@users.noreply.github.com>
2025-09-17 20:29:48 +0100

    Update regression.py

--- src/conformity/regression.py
+++ src/conformity/regression.py
@@ -11,24 +11,21 @@ class ConformalRegressor(BaseConformalPredictor):
         self.estimator = estimator
 
     def calibrate(self, X: ArrayLike, y: ArrayLike) -> None:
-        # predict class probabilities
         y_pred = self.estimator.predict(X)
 
-        # conformity measure - hinge loss in this case
         self.calibration_non_conformity = np.abs(y - y_pred)
         self.n_calib = self.calibration_non_conformity.shape[0]
 
         return None
 
     def predict(self, X: ArrayLike, alpha: float = 0.05) -> ArrayLike:
-        # predict class probabilities
         y_pred = self.estimator.predict(X)
-        # compute empirical conformity distribution
+
         y_pred_q_level = np.quantile(
             self.calibration_non_conformity,
             np.ceil((self.n_calib + 1) * (1 - alpha)) / self.n_calib,
         )
-        # create prediction intervals
+
         y_pred_lower = y_pred - y_pred_q_level
         y_pred_higher = y_pred + y_pred_q_level
 
